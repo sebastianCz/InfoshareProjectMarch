87edd89dcc6ee1d00d2f21a349765149da8bda58
Michał Tracichleb <Michal.Tracichleb@gmail.com>
2022-09-24 10:41:58 +0200

    Initial commit

---
+++ OstreC/OstreC/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace OstreC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                //Wprowadzenie do gry - Sebastian
+
+
+                //Tworzenie postaci -Arek
+                do
+                {
+
+                } while (true);
+
+                //Wprowadznie do fabuły - puste
+
+
+                //Wybór typ paragrafu - Michał
+                
+                int id = int.Parse(Console.ReadLine());
+
+                switch (id)
+                {
+                    case 0: //Walka - Piotr
+                        break;
+                    case 1: //Dialog - Tomek
+                        break;
+                }
+
+            } while (true);
+        }
+    }
+}
