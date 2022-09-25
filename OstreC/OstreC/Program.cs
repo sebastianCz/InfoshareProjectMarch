88c6e3b1b85acc16d0e3effa1d1ccb15f336af3a
Michał Tracichleb <Michal.Tracichleb@gmail.com>
2022-09-25 12:40:46 +0200

    Added - Choose your race

--- OstreC/OstreC/Program.cs
+++ OstreC/OstreC/Program.cs
@@ -39,8 +39,8 @@ namespace OstreC
             Console.WriteLine(playerSerialized);
 
             var currentDirectory = Directory.GetCurrentDirectory();
-            var playerSerializedPath = Path.Combine(currentDirectory, "data", "data", "player.json");
-            var racesSerializedPath = Path.Combine(currentDirectory, "data", "data", "races.json");
+            var playerSerializedPath = Path.Combine(currentDirectory, "data", "dataJson", "player.json");
+            var racesSerializedPath = Path.Combine(currentDirectory, "data", "dataJson", "races.json");
 
             //Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "data"));
 
@@ -58,15 +58,85 @@ namespace OstreC
 
             RacesList races = JsonConvert.DeserializeObject<RacesList>(racesSerialized);
 
-            Console.WriteLine(races.Results[3].Name);
-            foreach (var asi in races.Results[3].Asi)
+            
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\n\nChoise your race: ");
+            Console.ResetColor();
+
+            int idRace = -1;
+            foreach (var race in races.Results)
+            {
+                idRace += 1;
+                Console.Write($"{idRace}. ");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine(race.Name);
+                Console.ResetColor();
+                Console.WriteLine(race.Desc);
+                Console.WriteLine(race.Asi_desc);
+                Console.WriteLine(race.Speed_desc);
+            }
+            
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.D0:
+                case ConsoleKey.NumPad0:
+                    idRace = 0;
+                    break;
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    idRace = 1;
+                    break;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    idRace = 2;
+                    break;
+                case ConsoleKey.D3:
+                case ConsoleKey.NumPad3:
+                    idRace = 3;
+                    break;
+                case ConsoleKey.D4:
+                case ConsoleKey.NumPad4:
+                    idRace = 4;
+                    break;
+                case ConsoleKey.D5:
+                case ConsoleKey.NumPad5:
+                    idRace = 5;
+                    break;
+                case ConsoleKey.D6:
+                case ConsoleKey.NumPad6:
+                    idRace = 6;
+                    break;
+                case ConsoleKey.D7:
+                case ConsoleKey.NumPad7:
+                    idRace = 7;
+                    break;
+                case ConsoleKey.D8:
+                case ConsoleKey.NumPad8:
+                    idRace = 7;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error, try again");
+                    Console.ResetColor();
+                    break;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"\n\nYour choice: {races.Results[idRace].Name}");
+            Console.ResetColor();
+            Console.WriteLine("Bonus to your Attributes");
+            
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (var asi in races.Results[idRace].Asi)
             {
                 foreach (var asi2 in asi.Attributes)
                 {
-                    Console.WriteLine(asi2);
-                    Console.WriteLine(asi.Value);
+                    Console.Write(asi2);
+                    Console.WriteLine($" +{asi.Value}");
                 }
             }
+            Console.ResetColor();
         }
     }
 }
