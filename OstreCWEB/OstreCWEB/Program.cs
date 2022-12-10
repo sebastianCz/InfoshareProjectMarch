606037c0a2cbd0e574addcd6deb8f76fbaa43064
Michał Tracichleb <michal.tracichleb@gmail.com>
2022-12-10 14:46:30 +0100

    Add RazorRuntimeCompilation

--- OstreCWEB/OstreCWEB/Program.cs
+++ OstreCWEB/OstreCWEB/Program.cs
@@ -12,7 +12,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services
+    .AddControllersWithViews()
+    .AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
