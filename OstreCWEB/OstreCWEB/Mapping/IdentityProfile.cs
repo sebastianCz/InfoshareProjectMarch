a77f97c57e5eb5e6ed7bdaf64e2711ffa7834f92
Michał Tracichleb <111907641+michal-tracichleb@users.noreply.github.com>
2023-01-15 11:41:35 +0100

    Fixed bug with StoryProfile

---
+++ OstreCWEB/OstreCWEB/Mapping/IdentityProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using OstreCWEB.Data.Repository.Identity;
+using OstreCWEB.ViewModel.Identity;
+
+namespace OstreCWEB.Mapping
+{
+    public class IdentityProfile : Profile
+    {
+        public IdentityProfile()
+        {
+            CreateMap<User, UserView>();
+        }
+    }
+}
