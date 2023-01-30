5e7a67a16d786251340895d0fedf25d9d05213be
Tomasz Salagacki <tsalagacki@gmail.com>
2023-01-30 21:33:53 +0000

    In progress email confirmation in Identity

--- OstreCWEB/OstreCWEB/Controllers/LoginController.cs
+++ OstreCWEB/OstreCWEB/Controllers/LoginController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Diagnostics;
 using OstreCWEB.Data.Repository.Identity;
 using OstreCWEB.Services.Identity;
+using System.Net.Mail;
+using System.Net;
 
 namespace OstreCWEB.Controllers
 {
@@ -66,30 +68,69 @@ namespace OstreCWEB.Controllers
         }
         [Authorize]
         [HttpPost]
-        public async Task<IActionResult>ChangePassword(ChangePassword model)
+        public async Task<IActionResult> ChangePassword(ChangePassword model)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                return View(model);
+                //return View(model);
                 var result = await _service.ChangePasswordAsync(model, User.Identity.Name);
                 TempData["msg"] = result.Message;
                 return RedirectToAction(nameof(ChangePassword));
             }
+            return View(model);
         }
-
-        // Admin account
-        //public async Task<IActionResult> Reg()
+        //public bool sendEmailSMTP(int emailType, CurrentUser currentUser, out string feedback)
         //{
-        //    var model = new Registration
+        //    var smtpClient = new SmtpClient("smtp.gmail.com")
         //    {
-        //        Username = "admin",
-        //        Name = "admin",
-        //        Email = "admin",
-        //        Password = "Admin@1234"
+        //        Port = 587,
+        //        Credentials = new NetworkCredential("ostreCGame@gmail.com", "jgkeyglxajjymsft"),
+        //        EnableSsl = true,
         //    };
-        //    model.Role = "admin";
-        //    var result = await this._service.RegistrationAsync(model);
-        //    return Ok(result);
-        //}
-    }
+
+        //    var mailMessage = new MailMessage
+        //    {
+        //        From = new MailAddress("ostreCGame@gmail.com"),
+        //        Subject = "",
+        //        Body = "",
+        //        IsBodyHtml = true,
+        //    };
+
+        //    //Forgot Password template
+        //    if (emailType == 1)
+        //    {
+        //        mailMessage.Subject = "Ostre C Game password recovery email";
+        //        mailMessage.Body = $"<h1>Hello,</h1> <br> <h2> dear {currentUser.UserName}</h2><br> You forgot your password. <br> For now the best I can do is send you your password. Here it is : <br>" +
+        //            $"Your username: {currentUser.UserName} <br> Your password: {currentUser.Password} <br> Please don't forget your password going forward. <br> <b>Regards</b>,<br><b> Ostre C team</b>";
+
+        //        mailMessage.To.Add(currentUser.Email);
+
+        //        feedback = "Email sent on the email adress assigned to your existing account :" + currentUser.Email;
+        //        smtpClient.Send(mailMessage);
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        feedback = "An non existing email template was chosen";
+        //        throw new Exception(feedback);
+        //        return false;
+
+        //    }
+
+            // Admin account
+            //public async Task<IActionResult> Reg()
+            //{
+            //    var model = new Registration
+            //    {
+            //        Username = "admin",
+            //        Name = "admin",
+            //        Email = "admin",
+            //        Password = "Admin@1234"
+            //    };
+            //    model.Role = "admin";
+            //    var result = await this._service.RegistrationAsync(model);
+            //    return Ok(result);
+            //}
+        }
+    
 }
