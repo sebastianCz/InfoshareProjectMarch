5e361880b1303f2ea54a5cec9bfb02496681b3da
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2023-02-03 22:17:04 +0100

    Removed fangehull solution.

--- Fangehull/Fangehull/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Fangehull.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
