e2621d0f1c72c786a51e861a0aa6fc1d462370b9
sebastian czarnowski <sebastianczarnowski95@gmail.com>
2023-02-03 18:47:52 +0100

    fixed login

--- OstreCWEB/OstreCWEB.Data/Repository/Identity/Registration.cs
+++ OstreCWEB/OstreCWEB.Data/Repository/Identity/Registration.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -10,18 +11,21 @@ namespace OstreCWEB.Data.Repository.Identity
     public class Registration
     {
         [Required]
+        [RegularExpression("^[A-Za-z.\\s_-]+$", ErrorMessage = "Minimum length 2, Max lenght 20 and can't contain special characters")]
         public string Name { get; set; }
         [Required]
         [EmailAddress]
         public string Email { get; set; } 
         [Required]
+        [RegularExpression("^[A-Za-z.\\s_-]+$", ErrorMessage = "Minimum length 2, Max lenght 20 and can't contain special characters")]
         public string UserName { get; set; }
         [Required]
         [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*[#$^+=!*()@%&]).{6,}$", ErrorMessage = "Minimum length 6 and must contain  1 Uppercase,1 lowercase, 1 special character and 1 digit")]
         public string Password { get; set; }
         [Required]
-        [Compare("Password")]
+        [Compare("Password", ErrorMessage = "Password confirmation invalid")]
         public string PasswordConfirm { get; set; }
+        [ValidateNever]
         public string ? Role { get; set; }
     }
 }
